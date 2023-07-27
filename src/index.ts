6782d7d9a6b8b512ceb044dabb4ee732b507c5c1
Stephen-PP <pstephen2016@fau.edu>
2023-07-27 18:15:00 -0400

    removed testing code

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { ProviderUtils } from "./utils/ProviderUtils";
 import { AppearanceService } from "./services/AppearanceService";
 import { LiquidityService } from "./services/LiquidityService";
 
-const contract = ERC20Contract.from("0x076a3e1500f3110d8f4445d396a3d7ca6d0ca269");
+/*const contract = ERC20Contract.from("0x076a3e1500f3110d8f4445d396a3d7ca6d0ca269");
 
 (async() => {
     console.log("starting");
@@ -18,7 +18,7 @@ const contract = ERC20Contract.from("0x076a3e1500f3110d8f4445d396a3d7ca6d0ca269"
     //const transfers = await AppearanceService.getAllTransfers("0x076a3e1500f3110d8f4445d396a3d7ca6d0ca269", "0xfb66321D7C674995dFcC2cb67A30bC978dc862AD", 17680100, 17680101);
 
     console.log(liq);
-})();
+})();*/
 
 const bot = new DiscordBot();
-//bot.login();
+bot.login();
